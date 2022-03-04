c82392ed9a36f3abb68d486504677c6cc083f7b6
manhdoan <doanmanh2907@gmail.com>
2022-03-04 19:51:58 +0700

    Revert "delete pages/api"
    This reverts commit 97c12f788544e3675f2889fc9a884cbabf4cfe07.

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
 const sass = require('@zeit/next-sass')
 
-module.exports = sass({
+module.exports = {
   reactStrictMode: true,
-  distDir: 'build',
-      exportPathMap: async function (
-        defaultPathMap,
-        { dev, dir, outDir, distDir, buildId }
-      ) {
-        return {
-          "/": { page: "/" },
-          "/blog": { page: "/blog" },
-          "/video": { page: "/video" }
-        };
-      }
-});
+}
