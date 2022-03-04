97c12f788544e3675f2889fc9a884cbabf4cfe07
manhdoan <doanmanh2907@gmail.com>
2022-03-04 19:31:53 +0700

    delete pages/api

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 const sass = require('@zeit/next-sass')
 
-module.exports = {
+module.exports = sass({
   reactStrictMode: true,
-}
+  distDir: 'build',
+      exportPathMap: async function (
+        defaultPathMap,
+        { dev, dir, outDir, distDir, buildId }
+      ) {
+        return {
+          "/": { page: "/" },
+          "/blog": { page: "/blog" },
+          "/video": { page: "/video" }
+        };
+      }
+});
